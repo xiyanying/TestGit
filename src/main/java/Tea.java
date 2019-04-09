c51086868e63e8eb51e22584b07b761d51afe8b5
xyy <xyy@hand.com>
2019-04-09 19:29:43 +0800

    switch hot-fix

--- src/main/java/Tea.java
+++ src/main/java/Tea.java
@@ -1,10 +1,7 @@
 
 public class Tea {
 	public void getTea(){
-<<<<<<< HEAD
 		System.out.println("1111");
-=======
 		System.out.println("2222");
->>>>>>> branch 'master' of https://github.com/xiyanying/TestGit.git
 	}
 }
